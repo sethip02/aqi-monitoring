bb217d160c7aae917ddad782b759afd22b34513d
prateek sethi <prateeksethi2989@gmail.com>
2021-07-07 23:31:47 +0530

    Added websocket events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+let webSocketObj = new WebSocket("ws://city-ws.herokuapp.com/");
 
 function App() {
+  const [aqiData, setAQIData] = useState([]);
+  
+  
+  
+  useEffect(() => {
+    webSocketObj.onopen = () => {
+    console.log("Connection established with the Server");
+    };
+    
+    webSocketObj.onmessage = (message) => {
+      console.log("Message received from the server : " + JSON.stringify(message));
+      setAQIData([...message]);
+    };
+
+    webSocketObj.onclose = () => {
+      console.log("Connection to the server is closed");
+    }
+    
+  }, []);
+
+
   return (
     <div className="App">
-      
+      <p>{ JSON.stringify(aqiData)}</p>
     </div>
   );
 }
